268b2cdcf672a753d24c06926c341987eac70ae5
mmanna3 <mmmannna3@gmail.com>
2025-02-08 22:47:30 -0300

    corrige 's' olvidada en vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/fichaje-fe/'
+  base: '/fichajes-fe/'
 })
