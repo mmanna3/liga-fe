beb9af9724696b2621838dd6158157862ea0dfd1
mmanna3 <mmmannna3@gmail.com>
2025-03-03 13:58:23 -0300

    Tabla jugador usando la genérica

--- src/pages/admin/jugador/tabla.tsx
+++ src/pages/admin/jugador/tabla.tsx
@@ -1,103 +1,65 @@
-import { JugadorDTO } from '@/api/clients'
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
-import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu'
-import { Skeleton } from '@/components/ui/skeleton'
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow
-} from '@/components/ui/table'
+import { api } from '@/api/api'
+import { EquipoDelJugadorDTO, JugadorDTO } from '@/api/clients'
+import useApiQuery from '@/api/custom-hooks/use-api-query'
+import Tabla from '@/components/ykn-ui/tabla'
 import { rutasNavegacion } from '@/routes/rutas'
-import { MoreVertical } from 'lucide-react'
+import { ColumnDef } from '@tanstack/react-table'
 import { useNavigate } from 'react-router-dom'
 
-type TablaProps = {
-  data: JugadorDTO[]
-  isLoading: boolean
-  isError: boolean
-}
-
-export default function Tabla({ data, isLoading, isError }: TablaProps) {
+export default function TablaJugador() {
   const navigate = useNavigate()
 
+  const { data, isLoading, isError } = useApiQuery({
+    key: ['jugadores'],
+    fn: async () => await api.jugadorAll()
+  })
+
+  const columnas: ColumnDef<JugadorDTO>[] = [
+    {
+      accessorKey: 'dni',
+      header: 'DNI',
+      cell: ({ row }) => <span>{row.getValue('dni')}</span>
+    },
+    {
+      accessorKey: 'nombre',
+      header: 'Nombre',
+      cell: ({ row }) => <span>{row.getValue('nombre')}</span>
+    },
+    {
+      accessorKey: 'apellido',
+      header: 'Apellido',
+      cell: ({ row }) => <span>{row.getValue('apellido')}</span>
+    },
+    {
+      accessorKey: 'equipos',
+      header: 'Equipos',
+      cell: ({ row }) => (
+        <span>{(row.getValue('equipos') as EquipoDelJugadorDTO[]).length}</span>
+      )
+    },
+    {
+      id: 'acciones',
+      header: '',
+      cell: ({ row }) => (
+        <Tabla.MenuContextual
+          items={[
+            {
+              texto: 'Detalle',
+              onClick: () =>
+                navigate(`${rutasNavegacion.detalleJugador}/${row.original.id}`)
+            }
+          ]}
+        />
+      )
+    }
+  ]
+
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>DNI</TableHead>
-          <TableHead>Nombre</TableHead>
-          <TableHead>Apellido</TableHead>
-          <TableHead>Equipos</TableHead>
-          <TableHead className='w-12' />
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {isError ? (
-          <TableRow>
-            <TableCell colSpan={2}>
-              <Alert variant='destructive' className='mb-4'>
-                <AlertTitle className='text-xl font-semibold'>Error</AlertTitle>
-                <AlertDescription>
-                  No se pudieron recuperar los datos de la tabla.
-                </AlertDescription>
-              </Alert>
-            </TableCell>
-          </TableRow>
-        ) : isLoading ? (
-          Array.from({ length: 3 }).map((_, index) => (
-            <TableRow key={index}>
-              <TableCell>
-                <Skeleton className='h-4 w-32' />
-              </TableCell>
-              <TableCell className='text-right'>
-                <Skeleton className='h-4 w-8' />
-              </TableCell>
-              <TableCell className='text-right'>
-                <Skeleton className='h-4 w-8' />
-              </TableCell>
-              <TableCell className='text-right'>
-                <Skeleton className='h-4 w-8' />
-              </TableCell>
-            </TableRow>
-          ))
-        ) : (
-          data.map((item) => (
-            <TableRow key={item.id}>
-              <TableCell>{item.dni}</TableCell>
-              <TableCell>{item.nombre}</TableCell>
-              <TableCell>{item.apellido}</TableCell>
-              <TableCell>{item.equipos!.length}</TableCell>
-              <TableCell className='text-right'>
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant='ghost' size='icon'>
-                      <MoreVertical className='w-5 h-5' />
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent align='end'>
-                    <DropdownMenuItem
-                      onClick={() =>
-                        navigate(`${rutasNavegacion.detalleJugador}/${item.id}`)
-                      }
-                    >
-                      Detalle
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </TableCell>
-            </TableRow>
-          ))
-        )}
-      </TableBody>
-    </Table>
+    <Tabla<JugadorDTO>
+      columnas={columnas}
+      data={data || []}
+      estaCargando={isLoading}
+      hayError={isError}
+    />
   )
 }
