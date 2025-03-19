4a9a5c5e582fb4cd7e3268ba11d7f67c3d5583a7
mmanna3 <mmmannna3@gmail.com>
2025-03-19 01:12:03 -0300

    agrega filtro a jugadores

--- src/pages/admin/jugador/jugador.tsx
+++ src/pages/admin/jugador/jugador.tsx
@@ -14,13 +14,21 @@ import { FilterIcon } from 'lucide-react'
 import { useState } from 'react'
 import Tabla from './tabla'
 
-const estadoConfig = {
-  [EstadoJugador.FichajePendienteDeAprobacion]: 'Pendiente de aprobación',
-  [EstadoJugador.FichajeRechazado]: 'Fichaje rechazado',
-  [EstadoJugador.Activo]: 'Activo',
-  [EstadoJugador.Suspendido]: 'Suspendido',
-  [EstadoJugador.Inhabilitado]: 'Inhabilitado'
-}
+// Cambiamos a un array de objetos para preservar el orden
+const estadoConfigArray = [
+  {
+    key: EstadoJugador.FichajePendienteDeAprobacion,
+    label: 'Pendiente de aprobación'
+  },
+  { key: EstadoJugador.FichajeRechazado, label: 'Fichaje rechazado' },
+  {
+    key: EstadoJugador.AprobadoPendienteDePago,
+    label: 'Aprobado pendiente de pago'
+  },
+  { key: EstadoJugador.Activo, label: 'Activo' },
+  { key: EstadoJugador.Suspendido, label: 'Suspendido' },
+  { key: EstadoJugador.Inhabilitado, label: 'Inhabilitado' }
+]
 
 export default function Jugador() {
   const [filtroEstados, setFiltroEstados] = useState<EstadoJugador[]>([])
@@ -51,19 +59,15 @@ export default function Jugador() {
             </Button>
           </PopoverTrigger>
           <PopoverContent className='w-60 flex flex-col gap-1'>
-            {Object.entries(estadoConfig).map(([key]) => (
+            {estadoConfigArray.map(({ key }) => (
               <div
                 key={key}
                 className={`cursor-pointer flex items-center gap-2 p-2 rounded-md ${
-                  filtroEstados.includes(key as unknown as EstadoJugador)
-                    ? 'bg-blue-100'
-                    : ''
+                  filtroEstados.includes(key) ? 'bg-blue-100' : ''
                 }`}
-                onClick={() => toggleFiltro(key as unknown as EstadoJugador)}
+                onClick={() => toggleFiltro(key)}
               >
-                <JugadorEquipoEstadoBadge
-                  estado={key as unknown as EstadoJugador}
-                />
+                <JugadorEquipoEstadoBadge estado={key} />
               </div>
             ))}
           </PopoverContent>
