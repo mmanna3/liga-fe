40134fde367e856166324cdf46f51eee15ec3dca
mmanna3 <mmmannna3@gmail.com>
2026-03-08 14:36:21 -0300

    refactor

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,23 @@
-import { TooltipProvider } from '@/components/ui/tooltip'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { TooltipProvider } from '@/components/ui/tooltip'
+import ErrorBoundary from './components/error-boundary'
 import { mapaRutasComponentes } from './routes/mapa-rutas-componentes'
+import './index.css'
 
+const queryClient = new QueryClient()
 const router = createBrowserRouter(mapaRutasComponentes)
 
-export default function App() {
-  return (
-    <TooltipProvider>
-      <RouterProvider router={router} />
-    </TooltipProvider>
-  )
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <RouterProvider router={router} />
+        </TooltipProvider>
+      </QueryClientProvider>
+    </ErrorBoundary>
+  </StrictMode>
+)
--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-export interface globalConstType {
-  BASE_URL: string
-}
-
-export const BASE_URL = 'http://0.0.0.0:5072/api'
