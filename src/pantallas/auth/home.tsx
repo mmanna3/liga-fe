ec4824aa022faa81c628a6f6476b188213086915
mmanna3 <mmmannna3@gmail.com>
2026-04-04 21:06:51 -0300

    "panel administrativo" en home

--- src/pantallas/auth/home.tsx
+++ src/pantallas/auth/home.tsx
@@ -1,11 +1,14 @@
 export default function Home() {
   return (
-    <div className='flex flex-1 min-h-0 items-center justify-center bg-[#05a459]'>
+    <div className='flex flex-col flex-1 min-h-0 items-center justify-center gap-4 bg-[#05a459]'>
       <img
         src='/ligas/edefi/icono.png'
         alt='Encuentro Deportivo de Fútbol Infantil'
         className='max-w-[420px] w-full object-contain'
       />
+      <h2 className='text-5xl font-bold text-green-900 ml-4'>
+        Panel Administrativo
+      </h2>
     </div>
   )
 }
